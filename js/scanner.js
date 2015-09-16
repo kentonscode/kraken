1122265d4a2c4608a0a89c24017c47a97c6069ad
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-16 13:52:08 -0700

    #11 working on printing scan results to file

--- js/scanner.js
+++ js/scanner.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var urlScan = express.Router(); 
+var fs = require('fs');
 
-
-urlScan.get('/scan', function(request, response, next) {
-  var spawn = require('child_process').spawn;
-  spawn('ruby', ['wpscan.rb', '--url', 'http://www.lemonandolives.com/'],
+urlScan.get('/scan/:url', function(request, response, next) {
+  var scan = require('child_process').spawn;
+  scan('ruby', ['wpscan.rb', '--url', request.params.url],
    {cwd: '/Users/Kentonkotsiris/wpscan/'}
    );
+
+  scan.stdout.on('data', function(data) {
+    fs.appendFile('message.txt', urlScan.stdout, function (err) {
+    // if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
+    
   response.send()
 });
+
 
 module.exports = urlScan;
