c7e6ac955ec036ca33131d68ac21ca78880219d1
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-16 12:50:52 -0700

    #11 going to /scan will initiate scan

--- js/scanner.js
+++ js/scanner.js
@@ -1,13 +1,13 @@
-var spawn = require('child_process').spawn;
+var express = require('express');
+var urlScan = express.Router(); 
 
-var scan = spawn('ruby', ['wpscan.rb', '--url', 'http://www.example.com/'],
- {cwd: '/Users/Kentonkotsiris/wpscan/'}
- );
 
-scan.stdout.on('data', function(data) {
-  console.log('The Kraken Is Released! ' + data);
+urlScan.get('/scan', function(request, response, next) {
+  var spawn = require('child_process').spawn;
+  spawn('ruby', ['wpscan.rb', '--url', 'http://www.lemonandolives.com/'],
+   {cwd: '/Users/Kentonkotsiris/wpscan/'}
+   );
+  response.send()
 });
 
-scan.on('exit', function (exitCode){
-  console.log('Kraken Scan Complete: ' + exitCode);
-}); 
+module.exports = urlScan;
