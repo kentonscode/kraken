fceea0caf49b61e2c2a63f48502d1015928987fc
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-17 16:21:35 -0700

    #12 working on altering reports display on client side

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function submitUrl(event) {
   event.preventDefault();
   console.log(formData()); //shows form inputs on client console 
   var requestScan = new XMLHttpRequest();
-  requestScan.open("get", "/scan/" + document.forms[0].elements.urlInput.value, true);
+  requestScan.open("GET", "/scan/" + document.forms[0].elements.urlInput.value, true);
   requestScan.setRequestHeader("Content-type", "application/json");
   requestScan.send(formData());
   requestScan.addEventListener('load', function(){
@@ -19,3 +19,12 @@ function formData() {
   theData.urlInput = document.forms[0].elements.urlInput.value;
   return JSON.stringify(theData);
 }
+
+// reports list
+var showReports = new XMLHttpRequest();
+showReports.open('GET', '/reports/')
+showReports.send();
+showReports.addEventListener('load', function(data){
+
+
+});
