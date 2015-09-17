64e111cc087348c8cc28e276e0f4d5be7abb6c5f
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-17 10:03:00 -0700

    updated app.js so kraken runs on server

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ var scannerRoute = require('./js/scanner.js');
 //scan route 
 app.use('/', scannerRoute);
 
+//middleware for images
+app.use('/img', express.static('img'));
 
-//don't forget this - it starts node server 
+//middleware for JS
+app.use(express.static('js'));
+
+//middleware for CSS
+app.use('/css', express.static('css'));
+
+//middleware for index
+app.use('/', express.static('./'));
+
+//express will listen on this port
 app.listen(1337);
