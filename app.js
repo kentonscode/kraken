22f7c0dd663466a03330e6d7971de0a6480a6139
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-17 13:01:31 -0700

    removed unneeded get request

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ app.use('/css', express.static('css'));
 //middleware for index
 app.use('/', express.static('./'));
 
-//middleware for scan
-app.get('/scan/:url', function(req, res) {
-  res.send(req.params.url);
-});
-
-
 
 //express will listen on this port
 app.listen(1337);
