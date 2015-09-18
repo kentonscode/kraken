a610ba7147107329886c72c8d10535e45315538b
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-18 14:50:11 -0700

    if/sle scanner reports dir

--- js/scanner.js
+++ js/scanner.js
@@ -12,11 +12,9 @@ urlScan.get('/scan/:url', function(request, response, next) {
   var dateScan = new Date().toDateString();
 
   myScan.stdout.on('data', function(data) {
-        if (__dirname === './reports')
-        //do something
-      else
-          (__dirname === './scanner.js'))
-
+        if (__dirname === './reports') {
+          process.chdir('./scanner.js');
+        }
     process.chdir('./reports'); //stores result in reports folder
     
     fs.appendFile(dateScan + ': ' + fileSystem + ': ' + request.params.url + ' - report.txt', data , function (err) {
