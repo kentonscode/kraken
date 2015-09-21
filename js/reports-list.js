d141fb821fa841b6cd4604391102502c799d3bb4
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-21 13:24:53 -0700

    alterting for loop

--- js/reports-list.js
+++ js/reports-list.js
@@ -6,14 +6,14 @@ showReports.addEventListener('load', function(data){
   var reportsText = JSON.parse(showReports.responseText);
   var splitReports = reportsText.toString().split(':');
   console.log(splitReports.length)
-  
-  for (var tr = 0; tr <= 2; tr++) {
+
+  for (var tr = 0; tr <= splitReports.length; tr++) {
     var tableRows = document.createElement('tr');
     document.getElementById('reports').appendChild(tableRows)
 
   for(i = 0; i < splitReports.length; i++){
     var tableDate = document.createElement('td');
-    tableDate.textContent = splitReports[i];
+    tableDate.textContent = splitReports[tr];
     document.getElementsByTagName('tr')[tr].appendChild(tableDate);
   }
 }
