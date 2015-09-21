8f3d9dbdaca366b3f785937259b180ff565d0cdf
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-21 16:15:44 -0700

    #12 user ID is turned into reports href so user can click to view report

--- js/reports-list.js
+++ js/reports-list.js
@@ -9,9 +9,21 @@ showReports.addEventListener('load', function(data){
   var reportItem = document.createElement('tr');
   document.getElementById('reports').appendChild(reportItem);
   for (var detail = 0; detail < row.length; detail++) {
-   var itemDetail = document.createElement('td');
-   itemDetail.textContent = row[detail];
-   reportItem.appendChild(itemDetail);
- }
+    var itemDetail = document.createElement('td');
+    if(detail == 1) {
+     var link = document.createElement('a');
+     link.setAttribute('href', 'http://localhost:1337/report/' + row[detail].trim());
+     link.textContent = 'report';
+     reportItem.appendChild(link);
+   } else {
+    itemDetail.textContent = row[detail];
+    reportItem.appendChild(itemDetail);
+
+    var links = document.getElementsByName('a');
+    console.log(links);
+  }
 }
+}
+
+
 });
