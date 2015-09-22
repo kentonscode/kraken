ec07852b4ccbd413a83bf4a220eab08a9efcdbc2
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-22 13:23:25 -0700

    removed unneeded path and console.log

--- js/reports.js
+++ js/reports.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var reports = express.Router();
 var fs = require('fs');
-var path = require('path'); 
 
 reports.get('/list', function(request, response) {
   fs.readdir(__dirname + "/../reports", function (err, files) {
     if (err) throw err;
-    console.log(files);
     response.send(files);
   });
 
