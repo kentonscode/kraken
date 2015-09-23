fbf51ebe100a1243e017e7b51e50807c8f18686a
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-23 11:14:58 -0700

    Revert "Revert "#13 added code that returns report text in node based on id""
    This reverts commit ad12af3f89a2b3c0679bcb84303441c19f8faf60.

--- js/reports.js
+++ js/reports.js
@@ -10,4 +10,20 @@ reports.get('/list', function(request, response) {
 
 });
 
-module.exports = reports;
+//path to show single report
+
+reports.get('/print/:id', function(request, response){
+ var id = request.params.id;
+ fs.readdir(__dirname + '/../reports', function(err, files) {
+   for (var i = 0; i < files.length; i++) {
+     console.log(files[i].indexOf(id));
+     if (files[i].indexOf(id) > -1) {
+       var report = fs.readFileSync(__dirname + "/../reports/" + files[i], "utf8");
+       response.send(report);
+     }
+   }
+ });
+});
+
+ module.exports = reports;
+
