71d6629eb5d259b2368522b5f548c53220f4f0ea
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-23 15:00:29 -0700

    #13 fixed whitespace and report shows in client console

--- js/reports-list.js
+++ js/reports-list.js
@@ -3,41 +3,50 @@ var showReports = new XMLHttpRequest();
 showReports.open('GET', '/report/list/');
 showReports.send();
 showReports.addEventListener('load', function(data){
+
  var reports = JSON.parse(showReports.responseText);
- 
+
  for (var report = 0; report < reports.length; report++) {
-  var row = (reports[report].split(':'));
-  var reportItem = document.createElement('tr');
-  document.getElementById('reports').appendChild(reportItem);
+   var row = (reports[report].split(':'));
+   var reportItem = document.createElement('tr');
+   document.getElementById('reports').appendChild(reportItem);
 
-  for (var detail = 0; detail < row.length; detail++) {
-    var itemDetail = document.createElement('td');
-    if(detail == 1) {
-     var link = document.createElement('a');
-     link.setAttribute('href', 'http://localhost:1337/report/print/' + row[detail].trim());
-     link.textContent = 'view report';
-     reportItem.appendChild(link);
-   } else {
-    itemDetail.textContent = row[detail];
-    reportItem.appendChild(itemDetail);
-  }
-}
-}
-
+   for (var detail = 0; detail < row.length; detail++) {
+     var itemDetail = document.createElement('td');
+     if(detail == 1) {
+       var link = document.createElement('a');
+       link.setAttribute('href', 'http://localhost:1337/report/print/' + row[detail].trim());
+       link.textContent = 'view report';
+       reportItem.appendChild(link);
+     } else {
+       itemDetail.textContent = row[detail];
+       reportItem.appendChild(itemDetail);
+     }
+   }
+ } 
+ addListeners();
 });
 
 //show single report
-var viewReportlinks = document.getElementsByName('a');
 
-function displaySingleReport(event) {
+function showReport(event) {
   event.preventDefault();
-  var viewReport = new XMLHttpRequest();
-  viewReport.open('GET', '/report/print/:id', true);
-  viewReport.send(report);
-  viewReport.addEventListener('load', function(data) {
-    console.log('hello');
-
+  var id = document.getElementsByTagName('a')[3].getAttribute('href').split('/').pop()
+  var report = new XMLHttpRequest();
+  report.open('GET', '/report/print/' + id , true);
+  report.send();
+  report.addEventListener('load', function() {
+     console.log(report.responseText)
   });
 }
 
-viewReportlinks.addEventListener('click', displaySingleReport, false);
+function addListeners() {
+ var reportLinks = document.getElementsByTagName('a')
+
+ for (var i = 0; i < reportLinks.length; i++) {
+   reportLinks[i].addEventListener('click', function(e) {
+     e.preventDefault();
+     showReport(e);
+   }, false);
+  }
+}
