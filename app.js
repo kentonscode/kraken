2b10ed7798c2387cc7911b5345e149f56d5289ff
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-25 11:00:18 -0700

    updated links to reflect new routes

--- app.js
+++ app.js
@@ -2,25 +2,22 @@ var express = require('express');
 var app = express();
 var scannerRoute = require('./js/scanner.js');
 var reportsRoute = require('./js/reports.js');
+var reportsListRoute = require('./js/reports-route.js');
+var membershipRoute = require('./js/membership-route.js');
+var userPageRoute = require('./js/user-page-route.js');
+var loginRoute = require('./js/login-route.js');
 
-
-//middleware for scan 
+app.use('/user-page', userPageRoute);
+app.use('/membership', membershipRoute);
 app.use('/scan', scannerRoute);
-
-//middleware for /report
 app.use('/report', reportsRoute);
+app.use('/reports-list', reportsListRoute);
+app.use('/login', loginRoute);
 
-//middleware for images
+
+app.use('/', express.static('./'));
 app.use('/img', express.static('img'));
-
-//middleware for JS
+app.use('/css', express.static('css'));
 app.use(express.static('js'));
 
-//middleware for CSS
-app.use('/css', express.static('css'));
-
-//middleware for index
-app.use('/', express.static('./'));
-
-//express will listen on this port
 app.listen(1337);
