d12bd7da26a5598351269824cc57d00ac55f86c8
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-25 09:29:22 -0700

    removed report.txt from report when saved

--- js/scanner.js
+++ js/scanner.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 urlScan.get('/:url', function(request, response, next) {
   var scan = require('child_process').spawn;
-  var myScan = scan('ruby', ['wpscan.rb', '--batch', '--no-color', '--no-banner', '--url', request.params.url],
+  var myScan = scan('ruby', ['wpscan.rb', '--batch', '--no-color', '--url', request.params.url],
    {cwd: '/Users/Kentonkotsiris/wpscan/'}
    );
 
@@ -16,7 +16,7 @@ urlScan.get('/:url', function(request, response, next) {
     if (currentDirectory.search('reports') == -1) {
         process.chdir('./reports'); //stores result in reports folder
       }
-      fs.appendFile(dateScan + ': ' + fileSystem + ': ' + request.params.url + ' - report.txt', data , function (err) {
+      fs.appendFile(dateScan + ': ' + fileSystem + ': ' + request.params.url, data , function (err) {
         if (err) throw err;
         console.log('new report scan file created!');
       });
