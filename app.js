f65edc675eb652458ca121b1a67797076a86d9a4
kentonscode@gmail.com <kentonscode@gmail.com>
2015-09-25 11:15:21 -0700

    reorganization to alphabetize modules

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
-var scannerRoute = require('./js/scanner.js');
+var loginRoute = require('./js/login-route.js');
+var membershipRoute = require('./js/membership-route.js');
 var reportsRoute = require('./js/reports.js');
 var reportsListRoute = require('./js/reports-route.js');
-var membershipRoute = require('./js/membership-route.js');
+var scannerRoute = require('./js/scanner.js');
 var userPageRoute = require('./js/user-page-route.js');
-var loginRoute = require('./js/login-route.js');
 
-app.use('/user-page', userPageRoute);
+app.use('/login', loginRoute);
 app.use('/membership', membershipRoute);
-app.use('/scan', scannerRoute);
 app.use('/report', reportsRoute);
 app.use('/reports-list', reportsListRoute);
-app.use('/login', loginRoute);
+app.use('/scan', scannerRoute);
+app.use('/user-page', userPageRoute);
 
 app.use('/', express.static('./'));
 app.use('/img', express.static('img'));
