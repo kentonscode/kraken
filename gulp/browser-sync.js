45060fbd70b2f9f318ab11ea4668da31de641920
kentonscode@gmail.com <kentonscode@gmail.com>
2015-10-02 16:01:56 -0700

    added gulp task to browser

--- gulp/browser-sync.js
+++ gulp/browser-sync.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
 //browsersync
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', ['minify-html'],  function() {
   browserSync.init({
     proxy: "localhost:1337"
   });
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
-var requireDir = require('require-dir');
+require('require-dir')('./gulp');  
 
 //run following when typing gulp
-gulp.task('default', ['nodemon', 'compress', 'minify-html', 'minify-css', 'optimize-img', 'browser-sync']);
-
-require('require-dir')('./gulp');  
+gulp.task('default', ['nodemon','browser-sync', 'optimize-img', 'compress', 'minify-css']);
